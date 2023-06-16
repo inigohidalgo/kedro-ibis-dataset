a8560b3319df939e7becdfcf3d7730b2f43c85fa
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2023-06-17 00:22:24 +0200

    doc: documented dataset init and create_connection

--- src/kedro_ibis_dataset/kedro_ibis_dataset.py
+++ src/kedro_ibis_dataset/kedro_ibis_dataset.py
@@ -3,10 +3,22 @@ import ibis
 
 
 class IbisDataSet(AbstractDataSet):
-    connections = {}
+    """``IbisDataSet`` loads and `TODO` data to a table in an Ibis connection.
+    When loading data, it returns an Ibis table which is a lazy connection to the data.
+    """
+    connections: dict[str, ibis.BaseBackend] = {}
 
-    def __init__(self, table_name, credentials):
+    def __init__(self, table_name: str, credentials: dict[str, str]):
+        """Creates a new instance of ``IbisDataSet`` pointing to a table in an Ibis connection.
+        The connection is created only once per connection string and shared across all instances.
 
+        Args:
+            table_name: The name of the table which will be returned when loading data.
+            credentials: A dictionary containing the connection string under the key "con".
+        
+        Raises:
+            DataSetError: When ``table_name`` is empty.
+        """
         if not table_name:
             raise DataSetError("'table_name' argument cannot be empty.")
 
@@ -19,10 +31,23 @@ class IbisDataSet(AbstractDataSet):
         self.create_connection(self.connection_string)
 
     @classmethod
-    def create_connection(cls, connection_string):
+    def create_connection(cls, connection_string: str):
+        """If a connection has not been created for the given connection string,
+        create a new connection and store it in the class variable ``connections``.
+
+        Args:
+            connection_string: The connection string to use for creating the connection.
+        
+        Returns:
+            None
+        """
         if connection_string not in cls.connections:
             cls.connections[connection_string] = ibis.connect(connection_string)
         return
+
+    # TODO: check table exists when instantiating connection?
+    # would this be desirable behavior?
+    # def _exists(self):
 
     def _load(self):
         con = self.connections[self.connection_string]
