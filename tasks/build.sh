41ad745430e21dff27b66d931cb1ec66b7bcc748
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2023-06-17 02:17:40 +0200

    add project scripts

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,3 +1,6 @@
 rm -rf dist/
+echo "Building package"
 pdm build
+
+echo "Uploading package to index"
 twine upload dist/*
--- tasks/tag-release.sh
+++ tasks/tag-release.sh
@@ -3,4 +3,5 @@ commit_id=$(git rev-parse HEAD)
 
 echo "Tagging commit \"$commit_id\" as tag \"$package_version\""
 git tag $package_version
+echo "Pushing tag \"$package_version\" to origin"
 git push origin $package_version
